43e537bd0c40e0d05a8ecb0200aaab927f597ae1
ChadLin9596 <chadlin.me03@g2.nctu.edu.tw>
2018-10-30 22:20:25 +0800

    update git ignore

--- turtles_ws/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/edxian/NCRL-students/turtles_ws/src
-# Build directory: /home/edxian/NCRL-students/turtles_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
